e7c2b84b4ea00e201bc0d1d96fff6df24f556857
Purevbayar <hali_uka_2t@yahoo.com>
2012-05-31 12:04:31 +0800

    Revert "Update 2012-05-31"
    This reverts commit d487b87baaa73ce6b73fe86d1e81ab829c79e419.

--- src/index.php
+++ src/index.php
@@ -11,35 +11,33 @@
 
 
 <script type="text/javascript">
-     function slideSwitch() 
-	 {
-        var $active = $('#slideshow IMG.active');
-		if ( $active.length == 0 ) $active = $('#slideshow IMG:last');
-			var $next =  $active.next().length ? $active.next()
-			: $('#slideshow IMG:first');
-			$active.addClass('last-active');
-			$next.css({opacity: 0.0})
-			.addClass('active')
-			.animate({opacity: 1.0}, 1000, 
-			function() 
-				{
-					$active.removeClass('active last-active');
-				});
-     }                        
+     function slideSwitch() {
+     var $active = $('#slideshow IMG.active');
+     if ( $active.length == 0 ) $active = $('#slideshow IMG:last');
+     var $next =  $active.next().length ? $active.next()
+        : $('#slideshow IMG:first');
+     $active.addClass('last-active');
+     $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
+                                     }
+
 $(function() {
     setInterval( "slideSwitch()", 5000 );
 });		
-</script>
+	</script>
 		<script type="text/javascript">
-			$(function() 
-				{	$("#1, #2, #3").lavaLamp({				
+        $(function() 
+			{	$("#1, #2, #3").lavaLamp({				
 				fx: "backout", 
 				speed: 700	});});
 		</script>	
 		<title>
 			Airport
 		</title>
-</head>
+	</head>
 		
 	<body>
 
@@ -52,10 +50,11 @@ $(function() {
 				<!-----HEADER_LEFT---------------------------------------------->
 				<div id = "header_left">
 					<div id="slideshow">
-						<img src="style/image/1.jpg" alt="Slideshow Image 2" class="active"/>    
-						<img src="style/image/2.jpg" alt="Slideshow Image 2" />
-						<img src="style/image/3.jpg" alt="Slideshow Image 3" />
-					</div>   
+    <img src="style/image/1.jpg" alt="Slideshow Image 2" class="active"/>    
+	<img src="style/image/2.jpg" alt="Slideshow Image 2" />
+    <img src="style/image/3.jpg" alt="Slideshow Image 3" />
+</div>   
+					
 				</div>
 			
 				<!-----HEADER_RIGHT--------------------------------------------->
@@ -65,48 +64,53 @@ $(function() {
 
 				<?php
 			         $link = mysql_connect('localhost','root','');
-					 if (!$link)	die('Could not connect to MySQL: ' . mysql_error());
-						if (!mysql_select_db('airport', $link))
-							{		
-								echo 'Could not select database>';
-								exit;
-							}			
-							mysql_query("SET character_set_results=utf8");
-							mysql_query("SET character_set_client=utf8");
-							mysql_query("SET character_set_connection=utf8");	
-							$sql="select* from airplane";
-							$result=mysql_query($sql);
-							while( $row = mysql_fetch_assoc($result) )
+			if (!$link)	die('Could not connect to MySQL: ' . mysql_error());
+			if (!mysql_select_db('airport', $link))
+			{		
+				echo 'Could not select database>';
+				exit;
+			}			
+			mysql_query("SET character_set_results=utf8");
+			mysql_query("SET character_set_client=utf8");
+			mysql_query("SET character_set_connection=utf8");	
+			
+				$sql="select* from airplane";
+								$result=mysql_query($sql);
+								while( $row = mysql_fetch_assoc($result) )
 								{
 									if($row['airplane_status']=="Land on" || $row['airplane_status']=="airborne")
 									{
-										echo ' '.$row['airplane_name'].' &nbsp &nbsp'.$row['airplane_direct'].' &nbsp &nbsp'.$row['airplane_position'].' ( '.$row['airplane_status'].' )</br> </br>';
+									echo ' '.$row['airplane_name'].' &nbsp &nbsp'.$row['airplane_direct'].' &nbsp &nbsp'.$row['airplane_position'].' ( '.$row['airplane_status'].' )</br> </br>';
 									}
 								}
-					?>
+						?>
 
+	
 						</marquee>
 				</div>
 				
 			</div>
 			
 			<!-----Navigation--------------------------------------------------->
-		<div id="content">
-			<nav>
-				<ul id="appleNav">
-					<li><a href="index.php" title="Home page">Home</a></li>
-					<li><a href="booking.php" target="mainWindow" title="Booking">Booking</a></li>
-					<li><a href="flight_info.php" title="fight info">Flight info</a></li>
-					<li><a href="rewards_discounts.php" title="Rewards and discounts">Discounts</a></li>
-					<li><a href="programs_services.php" title="Programs and services">Services</a></li>
-					<li><a href="about.php" title="About us">About us</a></li>
-				</ul>
-			</nav>
+			   <div id="content">
+	    <nav>
+            <ul id="appleNav">
+
+			     <li><a href="index.php" title="Home page">Home</a></li>
+				<li><a href="booking.php" target="mainWindow" title="Booking">Booking</a></li>
+				<li><a href="flight_info.php" title="fight info">Flight info</a></li>
+                <li><a href="rewards_discounts.php" title="Rewards and discounts">Discounts</a></li>
+                <li><a href="programs_services.php" title="Programs and services">Services</a></li>
+                <li><a href="about.php" title="About us">About us</a></li>
+
+            </ul>
+		</nav>
 		</div>
 			
 		
 			<!-----MAIN--------------------------------------------------------->
 			<div id = "main">
+				
 				<!-----MAIN_LEFT------------------------------------------------>
 				<div id = "main_left">	
 					<iframe src="ticket_search.php">
@@ -116,7 +120,7 @@ $(function() {
 				<!-----MAIN_MIDDLE---------------------------------------------->
 				<div id = "main_middle">
 	                     <iframe src="homenews.php">
-						 </iframe>
+					</iframe>
 				</div>
 				
 				<!-----MAIN_RIGHT------------------------------------------------>
@@ -124,7 +128,8 @@ $(function() {
 				
 					<!-----MAIN_TOP---------------------------------------------->
 					<div id = "main_top">
-					       <iframe src="log.html"> </iframe>
+					 <iframe src="log.html">
+					</iframe>
 					</div>
 					
 					<!-----MAIN_BOTTOM------------------------------------------->
@@ -149,6 +154,7 @@ $(function() {
 			<!-----Footer------------------------------------------------------->
 			<div id = "footer">
 				<ul class="footermenu" >
+					
 					<li class="wordli"> <a href="#"><b> ABOUT US </b>      </a> </li>
 					<li > <a href=""> |        </a> </li>
 					<li class="wordli"> <a href="#"> <b>CONDITIONS OF CARRIAGE</b></a> </li>
